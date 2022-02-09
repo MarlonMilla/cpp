9e37aa6ef37419b9785a1ebe8c34ac6b195a23d4
Marlon Milla <Marlon_diaz02@outlook.com>
2022-02-08 18:33:47 -0600

    agregando tarea 2

---
+++ Ejercicios/Ejercicio9- Tarea 2 Factura exenta/factura.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    // Datos de entrada
+    double subtotal = 0;
+    double total = 0;
+    double impuesto = 0.15;
+    int descuento = 0;
+    double calculoDescuento = 0;
+    double calculoImpuesto = 0;
+    char estaExenta;
+
+    cout << "Ingrese el subtotal de la factura: ";
+    cin >> subtotal;
+
+    cout << "Ingrese el descuento (0, 10, 15, 20): ";
+    cin >> descuento;
+
+    cout << "Factura exenta S o N: ";
+    cin >> estaExenta;
+
+    // Proceso
+    if (estaExenta == 's' || estaExenta == 'S'){
+        total = subtotal;
+    }
+        
+    else{
+        calculoDescuento = (subtotal * descuento)/100;
+         calculoImpuesto = (subtotal - calculoDescuento) * 0.15;
+            total = subtotal - calculoDescuento  + calculoImpuesto;
+    }
+
+    // Salida
+
+    cout <<endl;
+    cout <<"Total a pagar: "<<total;
+
+    return 0;
