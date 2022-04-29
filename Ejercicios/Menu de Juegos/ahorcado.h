fbac53040547327347d9a8cd1d5ca7dbac865634
MarlonMilla <98498467+MarlonMilla@users.noreply.github.com>
2022-04-29 15:27:11 +0200

    Update ahorcado.h

--- Ejercicios/Menu de Juegos/ahorcado.h
+++ Ejercicios/Menu de Juegos/ahorcado.h
@@ -1,9 +1,14 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <string.h>
+#include <time.h>
+#include <conio.h>
 
 using namespace std;
 
-void ahorcado();
-void categorias();
-void ingresoCategoria (int op);
 void empezarJuego (char palabras[][15], char nombre[]);
 void dibujo (int intentos);
+void ingresoCategoria (int op);
+void categorias();
