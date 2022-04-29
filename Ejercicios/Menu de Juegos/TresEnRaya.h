ab340f09ebc5edbd438e2cc44f9d4747c2f841d7
MarlonMilla <98498467+MarlonMilla@users.noreply.github.com>
2022-04-29 15:30:29 +0200

    Update and rename tresEnRaya.h to TresEnRaya.h

---
+++ Ejercicios/Menu de Juegos/TresEnRaya.h
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <conio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+
+void armarTablero();
+void actualizarTurno(int x, int y);
+void identificarGanador(int turnoanterior);
